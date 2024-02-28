f378667cb4f4858f1bca7b7422fb6c10dc13bcbb
Dominic Hannah <dominic.hannah@education.nsw.gov.au>
2024-02-28 22:34:16 +1100

    blueback :

--- src/components/Triangle.svelte
+++ src/components/Triangle.svelte
@@ -2,26 +2,33 @@
   // I'm using typescript
   // let angles = $state([0, 0, 0]); // using svelte 5 runes for fun - Had to disable for astro
   // let triangle: string = $state(""); // actually anotating types here - again disabled for astro (it uses svelte v4)
-  let angles = [0, 0, 0];
+  let angles: number[] = [0, 1, 2];
   let triangle: string = "";
 
-  const handleInputChange = (index: any, event: any) => {
+  const handleInputChange = (i: any, e: any) => {
     // Yes this is lazy typescript
-    const value = parseInt(event.target.value, 10); // No idea why I need the radix, otherwise it doesn't use base 10 for whatever reason
-    if (!isNaN(value)) {
-      angles[index] = value;
+    const value = parseInt(e.target.value, 10); // No idea why I need the radix, otherwise it doesn't use base 10 for whatever reason
+    if (!isNaN(value)) { // If is number
+      angles[i] = value; // set angle to input
     } else {
-      triangle = "Angle must be an integer";
+      triangle = "Angle must be an integer"; // should probably make a try catch to stop runtime errors
     }
   };
 
-  const checkEquilateral = () => {
-    if (angles[0] === angles[1] && angles[1] === angles[2]) {
+  const checkEquilateral = () => { // es6 function as they look nicer
+    if (angles[0] === angles[1] && angles[1] === angles[2] && angles[0] + angles[1] + angles[3] === 180) { // Using ===
       triangle = "Equilateral triangle";
     } else {
       triangle = "Not an equilateral triangle";
     }
-  };
+     };
+ const checkScalene = () => {
+      if (angles) {
+        
+      }
+    }
+
+
 </script>
 
 <main>
@@ -37,3 +44,4 @@
   <button on:click={checkEquilateral}>Check Equilateral</button>
   <p>{triangle}</p>
 </main>
+
---
+++ src/components/Triangle.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Triangle = () => {
+
+    const [angles, setAngles] = useState([0, 1, 2]);
+    const [triangle, setTriangle] = useState("");
+    return (
+        <>
+            <input />
+            <input />
+            <input />
+            <p>{triangle}</p>
+        </>
+    )
+}
+
+export default Triangle;
